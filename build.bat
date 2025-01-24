74c6e44271f1f627699e8cc94255d85cce6da2bd
Miles Wijeyeratne <milesjpwijeyeratne@gmail.com>
2025-01-24 09:46:58 +0000

    fixed testing system

--- build.bat
+++ build.bat
@@ -20,23 +20,33 @@ cd %BUILD_DIR%
 :: config CMake
 cmake .. -G "Visual Studio 17 2022" -A x64
 if %errorlevel% neq 0 (
-    echo "CMake configuration failed!"
+    echo CMake configuration failed!
+    exit /b %errorlevel%
+)
+
+:: build common tests
+cmake --build . --config %CONFIG% --target common_tests
+if %errorlevel% neq 0 (
+	echo Common tests failed to build!
     exit /b %errorlevel%
 )
 
 :: run common tests
-cmake --build . --config %CONFIG% --target common_tests
+echo Running tests...
+common\%CONFIG%\common_tests.exe
 if %errorlevel% neq 0 (
-	echo "Common tests failed!"
+	echo Common tests failed!
     exit /b %errorlevel%
+) else (
+	echo All tests passed!
 )
 
 :: build target
 cmake --build . --config %CONFIG% --target %TARGET%
 if %errorlevel% neq 0 (
-    echo "Client build failed!"
+    echo Client build failed!
     exit /b %errorlevel%
 )
 
-echo "Build completed successfully!"
+echo Build completed successfully!
 exit /b 0
--- common/tests/combo.cpp
+++ common/tests/combo.cpp
@@ -7,9 +7,16 @@ using namespace combo;
 using namespace card;
 
 int main() {
+	bool all_passed = true;
+
 	Combo single_two(std::vector{Card(TWO, Suit::clubs)});
 	Combo single_ace(std::vector{Card(ACE, Suit::clubs)});
-	std::cout << "High two should beat high ace: " << single_two.beats(single_ace) << "\n";
+	if (!single_two.beats(single_ace)) {
+		std::cout << "High two should beat high ace: " << single_two.beats(single_ace) << "\n";
+		all_passed = false;
+	}
 
 	std::cout.flush();
+
+	return all_passed ? 0 : 1;
 }
